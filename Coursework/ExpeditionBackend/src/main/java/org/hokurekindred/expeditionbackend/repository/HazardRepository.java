72818f1356416144eae6f39222a49bcfe80e111c
Kindred <0059nai@gmail.com>
2024-10-30 17:22:59 +0300

    fix bug in HazardRepository

--- Coursework/ExpeditionBackend/src/main/java/org/hokurekindred/expeditionbackend/repository/HazardRepository.java
+++ Coursework/ExpeditionBackend/src/main/java/org/hokurekindred/expeditionbackend/repository/HazardRepository.java
@@ -1,6 +1,7 @@
 package org.hokurekindred.expeditionbackend.repository;
 
+import org.hokurekindred.expeditionbackend.model.Hazard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HazardRepository extends JpaRepository<HazardRepository, Long> {
+public interface HazardRepository extends JpaRepository<Hazard, Long> {
 }
