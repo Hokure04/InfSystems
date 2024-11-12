ec2263ada8aa6570b2098d92ed261de0429c7a94
ASUS <hokure.04@gmail.com>
2024-11-12 11:44:56 +0300

    equipment service and controller added same as vehicle service and controller

---
+++ Coursework/ExpeditionBackend/src/main/java/org/hokurekindred/expeditionbackend/controller/EquipmentController.java
@@ -0,0 +1,93 @@
+package org.hokurekindred.expeditionbackend.controller;
+
+import jakarta.validation.Valid;
+import org.hokurekindred.expeditionbackend.model.Equipment;
+import org.hokurekindred.expeditionbackend.service.EquipmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/equipment")
+public class EquipmentController {
+    @Autowired
+    private EquipmentService equipmentService;
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getAllEquipment(){
+        Map<String, Object> response = new HashMap<>();
+        response.put("equipment_list", equipmentService.findAllEquipments());
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> getEquipmentById(@PathVariable Long id){
+        Map<String, Object> response = new HashMap<>();
+        if (equipmentService.findEquipmentById(id).isPresent()) {
+            response.put("equipment", equipmentService.findEquipmentById(id));
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }
+        response.put("error", String.format("Equipment with id %d not found", id));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> createEquipment(@Valid @RequestBody Equipment equipment){
+        Map<String, Object> response = new HashMap<>();
+        try {
+            equipmentService.saveEquipment(equipment);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            response.put("error", "Error creating equipment");
+        }
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> deleteEquipment(@PathVariable Long id){
+        Map<String, Object> response = new HashMap<>();
+        try {
+            equipmentService.deleteEquipment(id);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+            response.put("error", "Error deleting equipment");
+        }
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Метод добавляет борудование в экспедицию
+    @PostMapping("/{equipmentId}/expeditions/{expeditionId}")
+    public ResponseEntity<Map<String, Object>> assignEquipment(@PathVariable Long equipmentId, @PathVariable Long expeditionId){
+        Map<String, Object> response = new HashMap<>();
+        if(equipmentService.assignEquipment(equipmentId, expeditionId)){
+            response.put("message", "Equipment assigned successfully");
+            response.put("status", HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }else{
+            response.put("Message", "Equipment already assigned");
+            response.put("status", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // Метод удаляет оборудование из экспедиции
+    @DeleteMapping("/{equipmentId}/expeditions/{expeditionId}")
+    public ResponseEntity<Map<String, Object>> removeVehicle(@PathVariable Long equipmentId, @PathVariable Long expeditionId){
+        Map<String, Object> response = new HashMap<>();
+        if(equipmentService.removeVehicle(equipmentId, expeditionId)){
+            response.put("message", "Equipment removed successfully");
+            response.put("status", HttpStatus.OK);
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        }else{
+            response.put("message", "Equipment not found");
+            response.put("status", HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+}
---
+++ Coursework/ExpeditionBackend/src/main/java/org/hokurekindred/expeditionbackend/service/EquipmentService.java
@@ -0,0 +1,78 @@
+package org.hokurekindred.expeditionbackend.service;
+
+import org.hokurekindred.expeditionbackend.model.Equipment;
+import org.hokurekindred.expeditionbackend.model.Expedition;
+import org.hokurekindred.expeditionbackend.repository.EquipmentRepository;
+import org.hokurekindred.expeditionbackend.repository.ExpeditionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EquipmentService {
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+
+    @Autowired
+    private ExpeditionRepository expeditionRepository;
+
+    public List<Equipment> findAllEquipments(){
+        return equipmentRepository.findAll();
+    }
+
+    public Optional<Equipment> findEquipmentById(Long id){
+        return equipmentRepository.findById(id);
+    }
+
+    public Equipment saveEquipment(Equipment equipment){
+        return equipmentRepository.save(equipment);
+    }
+
+    public Equipment updateVehicle(Long equipmentId, Equipment changedVehicle) {
+        return equipmentRepository.findById(equipmentId)
+                .map(equipment -> {
+                    equipment.setName(changedVehicle.getName());
+                    equipment.setType(changedVehicle.getType());
+                    equipment.setDescription(changedVehicle.getDescription());
+                    equipment.setStatus(changedVehicle.getStatus());
+                    equipment.setReservation(changedVehicle.getReservation());
+                    equipment.setPrice(changedVehicle.getPrice());
+                    return equipmentRepository.save(equipment);
+                })
+                .orElseThrow(() -> new RuntimeException("Equipment not found"));
+
+    }
+
+    public void deleteEquipment(Long id){
+        equipmentRepository.deleteById(id);
+    }
+
+    public boolean assignEquipment(Long equipmentId, Long expeditionId){
+        Equipment equipment = equipmentRepository.findById(equipmentId)
+                .orElseThrow(() -> new RuntimeException("Equipment not found"));
+        Expedition expedition = expeditionRepository.findById(expeditionId)
+                .orElseThrow(() -> new RuntimeException("Expedition not found"));
+
+        if(!equipment.getExpeditionList().contains(expedition)){
+            equipment.getExpeditionList().add(expedition);
+            equipmentRepository.save(equipment);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean removeVehicle(Long equipmentId, Long expeditionId){
+        Equipment equipment = equipmentRepository.findById(equipmentId)
+                .orElseThrow(() -> new RuntimeException("Vehicle not found"));
+        Expedition expedition = expeditionRepository.findById(expeditionId)
+                .orElseThrow(() -> new RuntimeException("Expedition not found"));
+
+        if(equipment.getExpeditionList().remove(expedition)){
+            equipmentRepository.save(equipment);
+            return true;
+        }
+        return false;
+    }
+}
